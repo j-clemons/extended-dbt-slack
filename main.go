f924eea8c94b939bfaaa26db3d65f0b48318770d
Jonathan <j-clemons@tuta.io>
2023-11-06 22:31:45 -0600

    add new functions for parsing logs

--- main.go
+++ main.go
@@ -4,13 +4,44 @@ import (
     "fmt"
     "os"
     "time"
+    "regexp"
+    "io"
 
     "github.com/joho/godotenv"
     "github.com/slack-go/slack"
 )
 
-func main() {
+type logPair struct {
+    Summary string
+    Detail  string
+}
 
+func parseLogs(logStr string) ([]string, []string) {
+    r, _ := regexp.Compile(`(?:ERROR creating).*?(?:\.\w{1,})`)
+    summary_lines := r.FindAllString(logStr, -1)
+    for i := range summary_lines {
+        fmt.Println(summary_lines[i])
+    }
+
+    details, _ := regexp.Compile(`(.*(Failure|Error) in .*\n.*\n.*)`)
+    detail_lines := details.FindAllString(logStr, -1)
+    for i := range detail_lines {
+        fmt.Println(detail_lines[i])
+    }
+
+    return summary_lines, detail_lines
+}
+
+func pairLogs(summaries []string, details []string) []logPair {
+    var results []logPair
+    for i := range summaries {
+       results = append(results, logPair(summaries[i], details[i]))
+    }
+
+    return results
+}
+
+func postMessage() {
     godotenv.Load(".env")
 
     token := os.Getenv("SLACK_AUTH_TOKEN")
@@ -39,5 +70,15 @@ func main() {
         panic(err)
     }
     fmt.Printf("Message sent at %s", timestamp)
+}
+
+func main() {
+
+    t, err := io.ReadAll(os.Stdin)
+    if err != nil {
+        panic(err)
+    }
+
+    parseLogs(string(t))
 
 }
