a64e2b6e627f0e2436c2c65ae0b4f8696dd7bda2
Jonathan <j-clemons@tuta.io>
2023-11-08 22:12:36 -0600

    split message and thread post

--- main.go
+++ main.go
@@ -3,18 +3,14 @@ package main
 import (
     "fmt"
     "os"
-    "time"
     "regexp"
+    "log"
     "io"
+    "strings"
 
     "github.com/joho/godotenv"
     "github.com/slack-go/slack"
 )
-
-type logPair struct {
-    Summary string
-    Detail  string
-}
 
 func parseLogs(logStr string) ([]string, []string) {
     r, _ := regexp.Compile(`(?:ERROR creating).*?(?:\.\w{1,})`)
@@ -32,44 +28,63 @@ func parseLogs(logStr string) ([]string, []string) {
     return summary_lines, detail_lines
 }
 
-func pairLogs(summaries []string, details []string) []logPair {
-    var results []logPair
-    for i := range summaries {
-       results = append(results, logPair(summaries[i], details[i]))
+func postMessageThread(threadTS string, detail string) {
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal(err)
     }
-
-    return results
-}
-
-func postMessage() {
-    godotenv.Load(".env")
 
     token := os.Getenv("SLACK_AUTH_TOKEN")
     channelID := os.Getenv("SLACK_CHANNEL_ID")
 
     client := slack.New(token, slack.OptionDebug(true))
 
-    attachment := slack.Attachment{
-        Pretext: "Better-dbt-bot",
-        Text:    "test",
-        Color:   "#36a64f",
-        Fields: []slack.AttachmentField{
-            {
-                Title: "Date",
-                Value: time.Now().String(),
-            },
-        },
-    }
+    codeBlock := "```" + detail + "```"
+
+    preTextField := slack.NewTextBlockObject("mrkdwn", codeBlock, false, false)
+    preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
 
     _, timestamp, err := client.PostMessage(
         channelID,
-        slack.MsgOptionAttachments(attachment),
+        slack.MsgOptionTS(threadTS),
+        slack.MsgOptionBlocks(preTextSection),
     )
 
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
     fmt.Printf("Message sent at %s", timestamp)
+}
+
+func postMessage(msg string) string {
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    token := os.Getenv("SLACK_AUTH_TOKEN")
+    channelID := os.Getenv("SLACK_CHANNEL_ID")
+
+    client := slack.New(token, slack.OptionDebug(true))
+
+    preTextField := slack.NewTextBlockObject("mrkdwn", msg, false, false)
+    preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
+
+    _, timestamp, err := client.PostMessage(
+        channelID,
+        slack.MsgOptionBlocks(preTextSection),
+    )
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("Message sent at %s", timestamp)
+
+    return timestamp
+}
+
+func formatMessages(msgLines []string) string {
+    return strings.Join(msgLines[:], "\n")
 }
 
 func main() {
@@ -79,6 +94,12 @@ func main() {
         panic(err)
     }
 
-    parseLogs(string(t))
+    summary, details := parseLogs(string(t))
+
+    ts := postMessage(formatMessages(summary))
+
+    for i := range details {
+        postMessageThread(ts, details[i])
+    }
 
 }
